5f781ff7c2a43500386a9dd3cd803661fd3039f5
abeershboul <abeer.shnoul@yahoo.com>
2022-08-26 22:04:51 +0300

    e

---
+++ src/comp/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Pa from "./pa";
+class App extends React.Component{
+  render(){
+    return(
+      <>
+      <Pa/>
+      </>
+    )
+  }
+}
+export default App;
